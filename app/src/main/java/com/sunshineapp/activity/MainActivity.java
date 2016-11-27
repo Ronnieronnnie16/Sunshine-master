c3be5c8cfbb3604ee3fcd3faa0a2ba1277286d95
Imron <ronnie_plsr@yahoo.com>
2016-11-27 12:01:45 +0700

    Initial commit

---
+++ app/src/main/java/com/sunshineapp/activity/MainActivity.java
@@ -0,0 +1,98 @@
+package com.sunshineapp.activity;
+
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import com.sunshineapp.R;
+import com.sunshineapp.adapter.CuacaRVAdapter;
+import com.sunshineapp.data.CuacaDBHelper;
+import com.sunshineapp.listener.OnCuacaClickListener;
+import com.sunshineapp.service.sync.SunshineSyncAdapter;
+
+public class MainActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<Cursor> ,
+                    OnCuacaClickListener{
+
+    public static final int RAMALAN_LOADER = 100;
+    RecyclerView mRecyclerView;
+    CuacaRVAdapter mCuacaAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        LinearLayoutManager llm = new LinearLayoutManager(MainActivity.this,
+                LinearLayoutManager.VERTICAL,false);
+
+        mRecyclerView.setLayoutManager(llm);
+        mCuacaAdapter = new CuacaRVAdapter(null, this);
+        mRecyclerView.setAdapter(mCuacaAdapter);
+
+        getLoaderManager().initLoader(RAMALAN_LOADER,null, this);
+
+        SunshineSyncAdapter.memulaiSync(this);
+
+//        Intent bukaRamalanService = new Intent(this, SunshineService.class);
+//        startService(bukaRamalanService);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        if (i == RAMALAN_LOADER) {
+            return new CursorLoader(
+                    MainActivity.this,
+                    Uri.parse("content://com.sunshineapp/ramalan"),
+                    new String[]{
+                            CuacaDBHelper.COLUMN_DT,
+                            CuacaDBHelper.COLUMN_MINTEMP,
+                            CuacaDBHelper.COLUMN_MAXTEMP,
+                            CuacaDBHelper.COLUMN_W_DESC,
+                            CuacaDBHelper.COLUMN_W_ICON
+                    },
+                    "ramalan.city_id = ?",
+                    new String[] {"1642911"},
+                    null
+            );
+        }
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        mCuacaAdapter.updateList(cursor);
+        mCuacaAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mCuacaAdapter.updateList(null);
+    }
+
+    @Override
+    public void onCuacaClick(int date) {
+        Intent intentBukaDetail = new Intent(MainActivity.this, DetailActivity.class);
+        intentBukaDetail.putExtra(DetailActivity.EXTRA_DATE, date);
+        this.startActivity(intentBukaDetail);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/sunshineapp/adapter/CuacaRVAdapter.java
@@ -0,0 +1,123 @@
+package com.sunshineapp.adapter;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
+import android.text.format.DateFormat;
+import android.text.format.Time;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+//import com.bumptech.glide.Glide;
+import com.squareup.picasso.Picasso;
+import com.sunshineapp.R;
+import com.sunshineapp.listener.OnCuacaClickListener;
+import com.sunshineapp.pojo.List;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
+/**
+ * Created by hendrysetiadi on 13/11/2016.
+ */
+
+public class CuacaRVAdapter extends RecyclerView.Adapter<CuacaRVAdapter.ViewHolder>{
+    public static final int TYPE_HEADER = 0;
+    public static final int TYPE_ITEM = 1;
+
+    Cursor cursor;
+    OnCuacaClickListener listener;
+    public CuacaRVAdapter (Cursor c, OnCuacaClickListener listener){
+        this.cursor = c;
+        this.listener = listener;
+    }
+    public void updateList(Cursor c){
+        this.cursor = c;
+    }
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        Context context = parent.getContext();
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        if (viewType == TYPE_HEADER) {
+            view = layoutInflater.inflate(R.layout.header_cuaca, parent, false);
+        }
+        else { // this is TYPE_ITEM
+            view = layoutInflater.inflate(R.layout.item_cuaca, parent, false);
+        }
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Context context = holder.ivCuaca.getContext();
+
+        cursor.moveToPosition(position);
+
+        Date date = new Date(Long.parseLong(cursor.getLong(0) +"000"));
+        holder.tvDate.setText(date.toString());
+        holder.tvLow.setText(String.format(
+                context.getString(R.string.xx_percent),
+                cursor.getDouble(1) ) );
+        holder.tvHigh.setText(String.format(
+                context.getString(R.string.xx_percent),
+                cursor.getDouble(2) ));
+        holder.tvCuacaDesc.setText(cursor.getString(3) );
+        // https://openweathermap.org/weather-conditions
+
+        // http://openweathermap.org/img/w/10d.png
+        Picasso.with(context).load("http://openweathermap.org/img/w/" +
+                cursor.getString(4)  + ".png").into(holder.ivCuaca);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (null!= cursor) {
+            return cursor.getCount();
+        }
+        return 0;
+//        return 10;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0){
+            return TYPE_HEADER;
+        }
+        else {
+            return TYPE_ITEM;
+        }
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder
+        implements View.OnClickListener{
+        TextView tvDate;
+        TextView tvLow;
+        TextView tvHigh;
+        TextView tvCuacaDesc;
+        ImageView ivCuaca;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ivCuaca = (ImageView) itemView.findViewById(R.id.iv_icon);
+            tvDate = (TextView) itemView.findViewById(R.id.tv_date);
+            tvLow = (TextView) itemView.findViewById(R.id.tv_low);
+            tvHigh = (TextView) itemView.findViewById(R.id.tv_high);
+            tvCuacaDesc = (TextView) itemView.findViewById(R.id.tv_icon_desc);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int position = getAdapterPosition();
+            cursor.moveToPosition(position);
+            listener.onCuacaClick((int)cursor.getLong(0));
+        }
+    }
+
+}
